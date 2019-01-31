27b19cbcb1d91c2bf5a4c41df0ecc299a3925263
ngriesser <nadege.griesser@1und1.de>
2019-01-31 18:55:57 +0100

    improve swagger documentation

--- src/main/java/de/griesser/outfit/restservice/api/RecommendationController.java
+++ src/main/java/de/griesser/outfit/restservice/api/RecommendationController.java
@@ -1,6 +1,8 @@
 package de.griesser.outfit.restservice.api;
 
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.DecimalMax;
@@ -17,12 +19,17 @@ public interface RecommendationController {
     String ERROR_SERVICE_UNAVAILABLE = "The service is currently unavailable, please try again later.";
     String ERROR_INTERNAL_SERVER_ERROR = "The service is currently unavailable, our developers are taking care of it.";
 
+    @ApiResponses(value = {@ApiResponse(code = 404, message = ERROR_CITY_NOT_FOUND),
+            @ApiResponse(code = 500, message = ERROR_INTERNAL_SERVER_ERROR),
+            @ApiResponse(code = 503, message = ERROR_SERVICE_UNAVAILABLE)})
     Recommendation getRecommendationByCityId(
             @ApiParam(value = "City Id (see city-contoller)", required = true, example = CITY_ID)
             @PositiveOrZero
                     long cityId);
 
 
+    @ApiResponses(value = {@ApiResponse(code = 500, message = ERROR_INTERNAL_SERVER_ERROR),
+            @ApiResponse(code = 503, message = ERROR_SERVICE_UNAVAILABLE)})
     Recommendation getRecommendationByCoordinates(
             @ApiParam(value = "Latitude in degrees (between -90 and 90)", required = true, example = LATITUDE)
             @DecimalMin("-90") @DecimalMax("90")
